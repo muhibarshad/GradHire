a2a28f7c88b94b49bf28a1bcccc551bdc9b6cf4c
Shafqat Abbas <chshafqat4422@gmail.com>
2025-03-21 00:10:18 +0500

    Discard Irrelevant Resumes Error Resolved

--- frontend/src/pages/JobDescription/JobDescription.js
+++ frontend/src/pages/JobDescription/JobDescription.js
@@ -1,5 +1,5 @@
-import { useState } from 'react'
-import { Link, useParams } from 'react-router-dom'
+import { useState } from "react";
+import { Link, useParams } from "react-router-dom";
 
 import {
   Card,
@@ -12,18 +12,18 @@ import {
   message,
   Col,
   Modal,
-} from 'antd'
+} from "antd";
 import {
   HeartOutlined,
   HeartFilled,
   TeamOutlined,
   DollarOutlined,
   ClockCircleOutlined,
-} from '@ant-design/icons'
+} from "@ant-design/icons";
 
-import styles from './JobDescription.module.css'
+import styles from "./JobDescription.module.css";
 
-import { useEffect } from 'react'
+import { useEffect } from "react";
 import {
   addFavourite,
   applyJob,
@@ -32,195 +32,207 @@ import {
   getJob,
   postApplication,
   removeFavourite,
-} from '../../util/api-call'
-import Spinner from '../Spinner'
-import { useDispatch } from 'react-redux'
-import { useSelector } from 'react-redux'
-import { updateUser } from '../../redux/user'
+} from "../../util/api-call";
+import Spinner from "../Spinner";
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
+import { updateUser } from "../../redux/user";
 
-const { Title, Text } = Typography
+const { Title, Text } = Typography;
 
 function JobDescription() {
-  const [job, setJob] = useState(null)
-  const [modal2Open, setModal2Open] = useState(false)
-  const user = useSelector((state) => state.user)
+  const [job, setJob] = useState(null);
+  const [modal2Open, setModal2Open] = useState(false);
+  const user = useSelector((state) => state.user);
 
-  const [messageApi, contextHolder] = message.useMessage()
-  const [loading, setLoading] = useState(false)
+  const [messageApi, contextHolder] = message.useMessage();
+  const [loading, setLoading] = useState(false);
 
-  const { id } = useParams()
-  const dispatch = useDispatch()
+  const { id } = useParams();
+  const dispatch = useDispatch();
 
-  const [applications, setApplications] = useState([])
+  const [applications, setApplications] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const res = await getApplication(user.id)
-        setApplications(res.data.data)
+        const res = await getApplication(user.id);
+        setApplications(res.data.data);
       } catch (err) {
         //console.log(err)
       }
-    }
-    fetchData()
-  }, [id])
+    };
+    fetchData();
+  }, [id]);
 
   useEffect(() => {
     const fetchJob = async () => {
       // Fetch job details using ID from location state or params
       try {
-        const res = await getJob(id)
-        setJob(res.data)
+        const res = await getJob(id);
+        setJob(res.data);
         //console.log(res.data)
       } catch (err) {
         //console.log(err)
       }
-    }
+    };
 
     const clickAdd = async () => {
       try {
-        const res = await clickJob(id)
+        const res = await clickJob(id);
         //console.log(res)
       } catch (err) {
         //console.log(err)
       }
-    }
+    };
 
-    fetchJob()
-    clickAdd()
-  }, [id])
+    fetchJob();
+    clickAdd();
+  }, [id]);
 
   // Add to favorites
   const handleAddFavoriteClick = async () => {
     try {
       // check if already added to favorites
       if (user.user.favouriteJobs.includes(job._id)) {
-        messageApi.error('Already added to favorites')
-        return
+        messageApi.error("Already added to favorites");
+        return;
       }
-      const res = await addFavourite(user.id, job._id)
-      if (res.status === 'success') {
+      const res = await addFavourite(user.id, job._id);
+      if (res.status === "success") {
         dispatch(
           updateUser({
             user: res.data.user,
           })
-        )
+        );
 
-        messageApi.success('Job added to favorites')
-      } else messageApi.error('Error ')
+        messageApi.success("Job added to favorites");
+      } else messageApi.error("Error ");
     } catch (err) {
       //console.log(err)
-      messageApi.error('Already Added to favorites')
+      messageApi.error("Already Added to favorites");
     }
-  }
+  };
 
   // Remove from favorites
   const handleRemoveFavoriteClick = async () => {
     try {
       // check if already added to favorites
       if (!user.user.favouriteJobs.includes(job._id)) {
-        messageApi.error('Not in favorites')
-        return
+        messageApi.error("Not in favorites");
+        return;
       }
 
-      const res = await removeFavourite(user.id, job._id)
-      if (res.status === 'success') {
+      const res = await removeFavourite(user.id, job._id);
+      if (res.status === "success") {
         dispatch(
           updateUser({
             user: res.data.user,
           })
-        )
-        messageApi.success('Job removed from favorites')
-      } else messageApi.success('Error')
+        );
+        messageApi.success("Job removed from favorites");
+      } else messageApi.success("Error");
     } catch (err) {
       //console.log(err)
-      messageApi.error('Error removing from favorites')
+      messageApi.error("Error removing from favorites");
     }
-  }
+  };
 
   const handleApplyClick = async () => {
-    // Check if the user has uploaded a resume
-    if (!user.user.resume || !user.user.resume.other || !user.user.resume.other.skills) {
-      messageApi.error('Please upload your resume with valid skills');
+    console.log("Resume Data: ", user.user.resume);
+    // console.log("Other Resume Data: ", user.user.resume.CV.other);
+    // console.log("Other Resume Data: ", user.user.resume.CV.other.skills);
+
+
+    if (
+      !user.user.resume ||
+      !user.user.resume.CV.other ||
+      !user.user.resume.CV.other.skills
+    ) {
+      messageApi.error("Please upload your resume with valid skills");
       return;
     }
-  
+
     // Extract skills from user resume (convert to array)
-    const userSkills = user.user.resume.other.skills
-      .split(' ')  // or you can split by commas if the skills are separated by commas
-      .map(skill => skill.trim().toLowerCase());  // Clean up any extra spaces
-  
-    const jobSkills = job.skills || [];
-    const jS = jobSkills.map(skill=>skill.toLowerCase());
-  
-    // Find the common skills between the user and job
-    const commonSkills = userSkills.filter(skill => jS.includes(skill));
-  
-    // Calculate the percentage of matching skills
-    const matchPercentage = (commonSkills.length / jobSkills.length) * 100;
-  
-    // Check if the match is greater than or equal to 50%
-    if (matchPercentage < 50) {
-      messageApi.error('Your skills do not match the job requirements. Application discarded.');
+    const userSkills = user.user.resume.CV.other.skills
+      .split(",") // <-- Change space to comma if skills are stored as "React, Node, HTML"
+      .map((skill) => skill.trim().toLowerCase());
+
+    // Convert job skills to lowercase for comparison
+    const jobSkills = (job.skills || []).map((skill) => skill.toLowerCase());
+
+    // Find the common skills
+    const commonSkills = userSkills.filter((skill) =>
+      jobSkills.includes(skill)
+    );
+
+    // Avoid division by zero
+    if (jobSkills.length === 0) {
+      messageApi.error("Job has no skills listed.");
       return;
     }
-  
+
+    // Calculate matching percentage
+    const matchPercentage = (commonSkills.length / jobSkills.length) * 100;
+
+    // Check if user meets the 50% threshold
+    if (matchPercentage < 50) {
+      messageApi.error(
+        `Your skills match only ${matchPercentage.toFixed(
+          2
+        )}% of the job requirements. Application discarded.`
+      );
+      return;
+    }
+
     try {
       setLoading(true);
-      const data = {
-        job: job._id,
-        user: user.id,
-      };
-  
+      const data = { job: job._id, user: user.id };
+
       // Post the application
       const res = await postApplication(data);
-  
-      if (res.status === 'success') {
-        const res2 = await applyJob(job._id);
-        messageApi.open({
-          type: 'success',
-          content: 'You have applied to this job successfully!',
-        });
-  
+
+      if (res.status === "success") {
+        await applyJob(job._id);
+        messageApi.success("You have applied to this job successfully!");
+
         setTimeout(() => setLoading(false), 1000);
         setTimeout(() => setApplications([...applications, res.data]), 1000);
       } else {
-        messageApi.error('Error applying to job');
+        messageApi.error("Error applying to job");
       }
     } catch (err) {
       setLoading(false);
-      console.log(err);
-      messageApi.error('Error applying to job');
-      setLoading(false);
+      console.error(err);
+      messageApi.error("Error applying to job");
     }
   };
-  
 
-  if (!job || loading || !user) return <Spinner />
+  if (!job || loading || !user) return <Spinner />;
 
-  const formattedDate = new Date(job.datePosted).toLocaleDateString('en-US', {
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric',
-  })
+  const formattedDate = new Date(job.datePosted).toLocaleDateString("en-US", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
 
   const tags = job.skills.map((skill) => (
-    <Tag key={skill} color='blue'>
+    <Tag key={skill} color="blue">
       {skill}
     </Tag>
-  ))
+  ));
 
   const favoriteIconAdd = (
-    <HeartOutlined style={{ color: '#ff4d4f', fontSize: '30px' }} />
-  )
+    <HeartOutlined style={{ color: "#ff4d4f", fontSize: "30px" }} />
+  );
   const favoriteIconRemove = (
-    <HeartFilled style={{ color: '#ff4d4f', fontSize: '30px' }} />
-  )
+    <HeartFilled style={{ color: "#ff4d4f", fontSize: "30px" }} />
+  );
 
-  console.log(applications)
+  console.log(applications);
   const hasApplied = applications.some(
     (application) => application.job._id === job._id
-  )
+  );
 
   return (
     <>
@@ -228,12 +240,12 @@ function JobDescription() {
       <div className={styles.wrapper}>
         <div className={styles.container}>
           <div className={styles.content}>
-            <Card style={{ width: '100%', borderRadius: '10px' }}>
+            <Card style={{ width: "100%", borderRadius: "10px" }}>
               <div
                 style={{
-                  display: 'flex',
-                  justifyContent: 'space-between',
-                  alignItems: 'center',
+                  display: "flex",
+                  justifyContent: "space-between",
+                  alignItems: "center",
                 }}
               >
                 <div>
@@ -246,16 +258,16 @@ function JobDescription() {
                   onClick={() => {
                     if (user) {
                       if (user.user.favouriteJobs.includes(job._id)) {
-                        handleRemoveFavoriteClick()
+                        handleRemoveFavoriteClick();
                       } else {
-                        handleAddFavoriteClick()
+                        handleAddFavoriteClick();
                       }
                     }
                   }}
                   title={
                     user.user.favouriteJobs.includes(job._id)
-                      ? 'Remove from favorites'
-                      : 'Add to favorites'
+                      ? "Remove from favorites"
+                      : "Add to favorites"
                   }
                 >
                   {user.user.favouriteJobs.includes(job._id)
@@ -267,77 +279,77 @@ function JobDescription() {
               <Row
                 gutter={[16, 16]}
                 style={{
-                  display: 'flex',
-                  justifyContent: 'space-between',
-                  alignItems: 'center',
+                  display: "flex",
+                  justifyContent: "space-between",
+                  alignItems: "center",
                 }}
               >
                 <Col xs={24} md={16}>
                   <Text
-                    style={{ fontSize: '18px' }}
+                    style={{ fontSize: "18px" }}
                   >{`${job.mode} / ${job.type}`}</Text>
                   <br />
-                  <Text style={{ fontSize: '16px' }}>{job.location}</Text>
+                  <Text style={{ fontSize: "16px" }}>{job.location}</Text>
                 </Col>
                 <Col xs={24} md={8}>
                   <div
                     style={{
-                      gap: '12px',
-                      display: 'flex',
-                      flexDirection: 'column',
-                      justifyContent: 'flex-start',
+                      gap: "12px",
+                      display: "flex",
+                      flexDirection: "column",
+                      justifyContent: "flex-start",
 
-                      textAlign: 'left',
+                      textAlign: "left",
                     }}
                   >
                     <div
                       style={{
-                        display: 'flex',
-                        flexDirection: 'row',
-                        justifyContent: 'flex-start',
-                        alignItems: 'center',
-                        gap: '10px',
+                        display: "flex",
+                        flexDirection: "row",
+                        justifyContent: "flex-start",
+                        alignItems: "center",
+                        gap: "10px",
                       }}
                     >
                       <DollarOutlined
                         style={{
-                          color: '#52c41a',
-                          fontSize: '16px',
+                          color: "#52c41a",
+                          fontSize: "16px",
                         }}
-                        class='iconStyle'
+                        class="iconStyle"
                       />
                       <Text>{job.salaryRange}</Text>
                     </div>
                     <div
                       style={{
-                        display: 'flex',
-                        flexDirection: 'row',
-                        justifyContent: 'flex-start',
-                        alignItems: 'center',
-                        gap: '10px',
+                        display: "flex",
+                        flexDirection: "row",
+                        justifyContent: "flex-start",
+                        alignItems: "center",
+                        gap: "10px",
                       }}
                     >
                       <TeamOutlined
                         style={{
-                          color: '#1890ff',
-                          fontSize: '16px',
+                          color: "#1890ff",
+                          fontSize: "16px",
                         }}
                       />
                       <Text>{`${job.noOfApplicants} applicants`}</Text>
                     </div>
                     <div
                       style={{
-                        display: 'flex',
-                        flexDirection: 'row',
-                        justifyContent: 'flex-start',
-                        alignItems: 'center',
-                        gap: '10px',
+                        display: "flex",
+                        flexDirection: "row",
+                        justifyContent: "flex-start",
+                        alignItems: "center",
+                        gap: "10px",
                       }}
                     >
                       <ClockCircleOutlined
                         style={{
-                          color: '#faad14',
-                          fontSize: '16px',
+                          color: "#faad14",
+                          fontSize: "16px",
                         }}
                       />
                       <Text>{formattedDate}</Text>
@@ -350,36 +362,36 @@ function JobDescription() {
               <Divider />
               <div>{tags}</div>
               <Divider />
-              <div style={{ textAlign: 'center' }}>
+              <div style={{ textAlign: "center" }}>
                 {hasApplied ? (
                   <Button
-                    style={{ width: '100%', marginBottom: '10px' }}
-                    type='primary'
-                    size='large'
+                    style={{ width: "100%", marginBottom: "10px" }}
+                    type="primary"
+                    size="large"
                     disabled
                   >
                     Already Applied
                   </Button>
                 ) : (
                   <Button
-                    style={{ width: '100%', marginBottom: '10px' }}
-                    type='primary'
-                    size='large'
+                    style={{ width: "100%", marginBottom: "10px" }}
+                    type="primary"
+                    size="large"
                     onClick={handleApplyClick} // replace handleApply with the actual apply logic
                   >
                     Apply
                   </Button>
                 )}
 
-                <Text type='primary'>
-                  By clicking the apply button, you agree to the{' '}
+                <Text type="primary">
+                  By clicking the apply button, you agree to the{" "}
                   <Link onClick={() => setModal2Open(true)}>
                     Terms & Conditions
-                  </Link>{' '}
+                  </Link>{" "}
                   of our website.
                 </Text>
                 <Modal
-                  title='Terms & Conditions'
+                  title="Terms & Conditions"
                   centered
                   open={modal2Open}
                   onOk={() => setModal2Open(false)}
@@ -436,7 +448,7 @@ function JobDescription() {
         </div>
       </div>
     </>
-  )
+  );
 }
 
-export default JobDescription
+export default JobDescription;
